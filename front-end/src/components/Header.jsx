3a05826a95a386251b8cc9d16fc28495b9705805
havencity-rp <khvedelidze.nik@gmail.com>
2025-07-24 19:57:58 +0400

    Update Header.jsx

--- front-end/src/components/Header.jsx
+++ front-end/src/components/Header.jsx
@@ -1,8 +1,18 @@
 import React from 'react'
+import styled from 'styled-components'
+
+const StyledHeaderWrapper = styled.div`
+height: 100px;
+width: 100%;
+background: black;
+`
 
 const Header = () => {
   return (
-    <div>Header</div>
+    <StyledHeaderWrapper>
+        <a href="/">Home</a>
+        <a href="/coffees">Coffees</a>
+    </StyledHeaderWrapper>
   )
 }
 
