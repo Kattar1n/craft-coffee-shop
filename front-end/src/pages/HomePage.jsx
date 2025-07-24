6348af4a4bd40b6bdf17e8f2b8ac3b43dbee4443
Ekaterine Danelia <139684673+Kattar1n@users.noreply.github.com>
2025-07-24 16:54:51 +0400

    herosection
    edited

--- front-end/src/pages/HomePage.jsx
+++ front-end/src/pages/HomePage.jsx
@@ -1,11 +1,18 @@
-import React from "react";
+import React from 'react';
+import Hero from '../components/Hero';
+import styled from 'styled-components';
+
+const HomePageContainer = styled.div`
+  /* Add styling for main layout of homepage if needed */
+`;
 
 const HomePage = () => {
+  return (
+    <HomePageContainer>
+      <Hero />
+      {/* You will add "Popular Coffees" section here later */}
+    </HomePageContainer>
+  );
+};
 
-return(
-    <div>
-        Hello World
-    </div>
-)}
-
-export default HomePage
+export default HomePage;
