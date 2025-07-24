698f85d3b42e50cdf8c38af6588d971def34fab6
havencity-rp <khvedelidze.nik@gmail.com>
2025-07-24 19:03:39 +0400

    Update HomePage.jsx

--- front-end/src/pages/HomePage.jsx
+++ front-end/src/pages/HomePage.jsx
@@ -1,18 +1,9 @@
-import React from 'react';
-import Hero from '../components/Hero';
-import styled from 'styled-components';
-
-const HomePageContainer = styled.div`
-  /* Add styling for main layout of homepage if needed */
-`;
+import React from 'react'
 
 const HomePage = () => {
   return (
-    <HomePageContainer>
-      <Hero />
-      {/* You will add "Popular Coffees" section here later */}
-    </HomePageContainer>
-  );
-};
+    <div>HomePage</div>
+  )
+}
 
-export default HomePage;
+export default HomePage
