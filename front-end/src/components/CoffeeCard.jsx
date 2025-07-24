bd942d91be016a2f1443a4fe03fe9314d50d9057
havencity-rp <khvedelidze.nik@gmail.com>
2025-07-24 17:56:50 +0400

    Update CoffeeCard.jsx

--- front-end/src/components/CoffeeCard.jsx
+++ front-end/src/components/CoffeeCard.jsx
@@ -1,5 +1,13 @@
 import React from "react";
-import { StyledCoffeeCard } from "./styled";
+import styled from "styled-components";
+
+const StyledCoffeeCard = styled.div`
+    width: 200px;
+    height: 300px;
+    background: rgba(0, 0, 0, 0.1);
+    text-align: center;
+    color: black;
+`
 
 const CoffeeCard = ({id, title, ingredientIds, description, isInStock}) => {
 
