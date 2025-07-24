d9773e666324ba270540a32cd3bbfd55875f4eb7
havencity-rp <khvedelidze.nik@gmail.com>
2025-07-24 19:57:41 +0400

    Update App.jsx

--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -1,26 +1,22 @@
 import React from 'react';
-import { Route, Routes} from 'react-router-dom';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import router from './router';
 
-import Layout from './layout/Layout';
-import HomePage from './pages/HomePage'
-import CoffeePage from './pages/CoffeePage';
-import CoffeeDetailPage from './pages/CoffeeDetailPage';
-import IngredientPage from './pages/IngredientPage';
-import IngredientDetailPage from './pages/IngredientDetailPage';
+
+// import Layout from './layout/Layout';
+// import HomePage from './pages/HomePage'
+// import CoffeePage from './pages/CoffeePage';
+// import CoffeeDetailPage from './pages/CoffeeDetailPage';
+// import IngredientPage from './pages/IngredientPage';
+// import IngredientDetailPage from './pages/IngredientDetailPage';
 
 
 function App() {
 
   return (
-    <Layout>
-      <Routes>
-        <Route path={'/'} Component={HomePage}/>
-        <Route path={'/coffees/'} Component={CoffeePage}/>
-        {/* <Route path={'/coffees/'} Component={CoffeeDetailPage}/> */}
-        <Route path={'/ingredients/'} Component={IngredientPage}/>
-        {/* <Route path={'/ingredients/'} Component={IngredientDetailPage}/> */}
-      </Routes>
-    </Layout>
+    <>
+      <RouterProvider router={createBrowserRouter(router)}/>
+    </>
   )
 }
 
