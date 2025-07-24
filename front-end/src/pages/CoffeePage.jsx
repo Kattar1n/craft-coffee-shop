5caf170b1387bfba8a25c9a3e3818e08cc4ef4ff
havencity-rp <khvedelidze.nik@gmail.com>
2025-07-24 17:57:04 +0400

    Update CoffeePage.jsx

--- front-end/src/pages/CoffeePage.jsx
+++ front-end/src/pages/CoffeePage.jsx
@@ -1,10 +1,11 @@
 import React from "react";
+import CoffeeWrapper from "../components/CoffeeWrapper";
 
 const CoffeePage = () => {
 
 return(
     <div>
-        
+        <CoffeeWrapper/>
     </div>
 )}
 
