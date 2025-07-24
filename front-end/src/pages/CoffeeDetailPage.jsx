00a793dc02eca3dc394941c24caf4a53dc423917
havencity-rp <khvedelidze.nik@gmail.com>
2025-07-24 23:24:36 +0400

    Update CoffeeDetailPage.jsx

--- front-end/src/pages/CoffeeDetailPage.jsx
+++ front-end/src/pages/CoffeeDetailPage.jsx
@@ -1,8 +1,21 @@
 import React from 'react'
+import { StyledPage } from './Styled'
+import Header from '../components/Header'
+import Footer from '../components/Footer'
+import { useParams } from 'react-router'
+import CoffeeDetailCard from '../components/CoffeeDetailCard'
+
+// import styled from "styled-components";
 
 const CoffeeDetailPage = () => {
+    const { coffeeId } = useParams()
+
   return (
-    <div>CoffeeDetailPage</div>
+    <StyledPage>
+        <Header/>
+        <CoffeeDetailCard key={coffeeId} id={coffeeId}/>
+        <Footer/>
+    </StyledPage>
   )
 }
 
