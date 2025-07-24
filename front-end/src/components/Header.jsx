108b60f920128bb50ba09c59e35bf69902a1ed9c
havencity-rp <khvedelidze.nik@gmail.com>
2025-07-24 17:56:54 +0400

    Update Header.jsx

--- front-end/src/components/Header.jsx
+++ front-end/src/components/Header.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import styled from 'styled-components';
 import { Link } from 'react-router-dom';
 import CoffeeShopLogo from './CoffeeShopLogo';
-import { useCurrency } from '../context/CurrencyContext';
+import { useCurrency } from './context/CurrencyContext';
 
 const HeaderContainer = styled.header`
   background-color: var(--color-primary);
