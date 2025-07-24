6f6bf9dc26a6a46e3b4b442be4246fb65b04d3fa
havencity-rp <khvedelidze.nik@gmail.com>
2025-07-24 16:17:56 +0400

    Update HomePage.jsx

--- front-end/src/pages/HomePage.jsx
+++ front-end/src/pages/HomePage.jsx
@@ -1,9 +1,15 @@
 import React from "react";
+import CoffeeCard from "../components/CoffeeCard";
 
-const HomePage = () => {
+const HomePage = async () => {
 
+    const coffees = await fetch("http://localhost:3000/posts/1")
+    .then()
+
+    console.log(coffees)
 return(
     <div>
+        <CoffeeCard title={"1"}></CoffeeCard>
         Hello World
     </div>
 )}
