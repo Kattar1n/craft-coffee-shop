c8cd4345a3cee22f56c63d8af8a59c21e67653e1
havencity-rp <khvedelidze.nik@gmail.com>
2025-07-24 23:24:25 +0400

    Update CoffeeWrapper.jsx

--- front-end/src/components/CoffeeWrapper.jsx
+++ front-end/src/components/CoffeeWrapper.jsx
@@ -2,10 +2,20 @@ import React, { useEffect, useState } from 'react'
 import styled from 'styled-components'
 import CoffeeCard from './CoffeeCard'
 
-const StyledCoffeeWrapper = styled.div`
+const StyledWrapper = styled.div`
+    color: #3E2723;
     width: 90%;
     margin: 0 auto;
     display: flex;
+    flex-direction: column;
+`
+const StyledCoffeeWrapper = styled.div`
+    margin: 0 auto;
+    width: 100%;
+    display: flex;
+    flex-direction: row;
+    flex-wrap: wrap;
+    gap: 20px;
     justify-content: space-between;
 `
 
@@ -19,9 +29,12 @@ const CoffeeWrapper = () => {
     }, [])
 
   return (
-    <StyledCoffeeWrapper>
-        {coffees.map((coffee) => <CoffeeCard key={coffee.id} id={coffee.id} title={coffee.title} ingredientIds={coffee.ingredientIds} description={coffee.description} isInStock={coffee.isInStock}/>)}
-    </StyledCoffeeWrapper>
+    <StyledWrapper>
+        <h1>ყავის სია</h1>
+        <StyledCoffeeWrapper>
+            {coffees.map((coffee) => <CoffeeCard key={coffee.id} id={coffee.id} title={coffee.title} ingredientIds={coffee.ingredientIds} description={coffee.description} isInStock={coffee.isInStock}/>)}
+        </StyledCoffeeWrapper>
+    </StyledWrapper>
   )
 }
 
