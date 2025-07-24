d16cab30726cf99e4bb56e8dc223f1949fc4f406
havencity-rp <khvedelidze.nik@gmail.com>
2025-07-24 17:37:30 +0400

    Update main.jsx

--- front-end/src/main.jsx
+++ front-end/src/main.jsx
@@ -1,12 +1,14 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.jsx';
+import { CurrencyProvider } from './context/CurrencyContext.jsx';
+import GlobalStyles from './styles/GlobalStyles.js';
 
-createRoot(document.getElementById('root')).render(
-  <App />
-)
-// createRoot(document.getElementById('root')).render(
-//   <StrictMode>
-//     <App />
-//   </StrictMode>,
-// )
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <GlobalStyles /> {/* Apply global styles first */}
+    <CurrencyProvider>
+      <App />
+    </CurrencyProvider>
+  </React.StrictMode>,
+);
