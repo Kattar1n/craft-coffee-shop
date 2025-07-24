54ff091a63888b36c16ab56d9255ea0e9b695bcc
havencity-rp <khvedelidze.nik@gmail.com>
2025-07-25 03:56:02 +0400

    Update IngredientPage.jsx

--- front-end/src/pages/IngredientPage.jsx
+++ front-end/src/pages/IngredientPage.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { StyledPage } from './Styled'
 import Header from '../components/Header'
 import Footer from '../components/Footer'
+import IngredientWrapper from '../components/IngredientWrapper'
 // import styled from "styled-components";
 
 const IngredientPage = () => {
@@ -9,7 +10,7 @@ const IngredientPage = () => {
     <StyledPage>
       <Header/>
       <main>
-        
+        <IngredientWrapper/>
       </main>
       <Footer/>
     </StyledPage>
