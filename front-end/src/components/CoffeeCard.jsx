341a548698595d05c085c63dd0b4a2b8f322b337
havencity-rp <khvedelidze.nik@gmail.com>
2025-07-24 16:17:46 +0400

    Update CoffeeCard.jsx

--- front-end/src/components/CoffeeCard.jsx
+++ front-end/src/components/CoffeeCard.jsx
@@ -1,2 +1,16 @@
 import React from "react";
+import { StyledCoffeeCard } from "./styled";
 
+const CoffeeCard = (id, title, ingredientIds, description, isInStock) => {
+
+return(
+    <StyledCoffeeCard>
+        <h2>{title}</h2>
+        <span>{ingredientIds}</span>
+        <span>{description}</span>
+        <span>{isInStock}</span>
+        <span>{id}</span>
+    </StyledCoffeeCard>
+)}
+
+export default CoffeeCard
