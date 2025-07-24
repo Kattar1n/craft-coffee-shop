c1857b1987499b32c5ae52d7dd1a0b3aed0cf3c6
havencity-rp <khvedelidze.nik@gmail.com>
2025-07-25 03:55:58 +0400

    Update IngredientDetailPage.jsx

--- front-end/src/pages/IngredientDetailPage.jsx
+++ front-end/src/pages/IngredientDetailPage.jsx
@@ -1,16 +1,19 @@
 import React from 'react'
 import { StyledPage } from './Styled'
+import { useParams } from 'react-router'
 import Header from '../components/Header'
 import Footer from '../components/Footer'
+import IngredientDetailCard from '../components/IngredientDetailCard'
 // import styled from "styled-components";
 
 const IngredientDetailPage = () => {
+  const { ingredientId } = useParams()
 
   return (
     <StyledPage>
         <Header/>
         <main>
-            
+            <IngredientDetailCard key={ingredientId} id={ingredientId}/>
         </main>
         <Footer/>
     </StyledPage>
