33b432609e51b65c2fe481ab1a2bc3b845d6625a
havencity-rp <khvedelidze.nik@gmail.com>
2025-07-24 23:24:50 +0400

    Update router.jsx

--- front-end/src/router.jsx
+++ front-end/src/router.jsx
@@ -1,15 +1,36 @@
+import Header from "./components/Header";
+import CoffeeDetailPage from "./pages/CoffeeDetailPage";
 import CoffeePage from "./pages/CoffeePage";
+import ErrorPage from "./pages/ErrorPage";
 import HomePage from "./pages/HomePage";
+import IngredientDetailPage from "./pages/IngredientDetailPage";
+import IngredientPage from "./pages/IngredientPage";
 
 const router = [
     {
         element: <HomePage/>,
-        path: '/'
+        errorElement: <ErrorPage/>,
+        index: '/'
     },
     {
         element: <CoffeePage/>,
-        path: '/coffees'
-    }
+        path: '/coffee'
+    },
+    {
+        element: <IngredientPage/>,
+        path: '/ingredients'
+    },
+    {
+        element: <CoffeeDetailPage/>,
+        path: '/coffee/:coffeeId'
+    },
+    {
+        element: <IngredientDetailPage/>,
+        path: '/ingredients/:ingredientId'
+    },
+    {
+        errorElement: <ErrorPage/>
+    },
 ]
 
 export default router
