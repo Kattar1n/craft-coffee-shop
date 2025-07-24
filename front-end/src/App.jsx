6ce6d9902239f184ddfce39d15850c1f74398c19
havencity-rp <khvedelidze.nik@gmail.com>
2025-07-24 19:03:26 +0400

    Update App.jsx

--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -1,10 +1,26 @@
+import React from 'react';
+import { Route, Routes} from 'react-router-dom';
+
+import Layout from './layout/Layout';
 import HomePage from './pages/HomePage'
+import CoffeePage from './pages/CoffeePage';
+import CoffeeDetailPage from './pages/CoffeeDetailPage';
+import IngredientPage from './pages/IngredientPage';
+import IngredientDetailPage from './pages/IngredientDetailPage';
+
+
 function App() {
 
   return (
-    <>
-    <HomePage/>
-    </>
+    <Layout>
+      <Routes>
+        <Route path={'/'} Component={HomePage}/>
+        <Route path={'/coffees/'} Component={CoffeePage}/>
+        {/* <Route path={'/coffees/'} Component={CoffeeDetailPage}/> */}
+        <Route path={'/ingredients/'} Component={IngredientPage}/>
+        {/* <Route path={'/ingredients/'} Component={IngredientDetailPage}/> */}
+      </Routes>
+    </Layout>
   )
 }
 
