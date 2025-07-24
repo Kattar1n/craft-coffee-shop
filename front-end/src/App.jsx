674db5f4885b9093f110fec0864b9cdeb6907220
havencity-rp <khvedelidze.nik@gmail.com>
2025-07-25 01:18:07 +0400

    Update App.jsx

--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -3,15 +3,6 @@ import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import router from './router';
 import './app.css'
 
-
-// import Layout from './layout/Layout';
-// import HomePage from './pages/HomePage'
-// import CoffeePage from './pages/CoffeePage';
-// import CoffeeDetailPage from './pages/CoffeeDetailPage';
-// import IngredientPage from './pages/IngredientPage';
-// import IngredientDetailPage from './pages/IngredientDetailPage';
-
-
 function App() {
 
   return (
