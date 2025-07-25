ede2adc2859b13312d9025b13ef7a59bb78940c4
havencity-rp <khvedelidze.nik@gmail.com>
2025-07-25 13:55:24 +0400

    Update HomePage.jsx

--- front-end/src/pages/HomePage.jsx
+++ front-end/src/pages/HomePage.jsx
@@ -2,14 +2,17 @@ import React from 'react'
 import { StyledPage } from './Styled'
 // import styled from "styled-components";
 import Header from '../components/Header'
+import Hero from '../components/Hero'
 import Footer from '../components/Footer'
+import CoffeeWrapper from '../components/CoffeeWrapper'
 
 const HomePage = () => {
   return (
     <StyledPage>
         <Header/>
         <main>
-            
+          <Hero/>
+          <CoffeeWrapper/>
         </main>
         <Footer/>
     </StyledPage>
